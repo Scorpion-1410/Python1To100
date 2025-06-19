e0ebfca8c8b24aef06ff9c627e380eb23bd4eaac
Scorpion-1410 <tanishq.trip14@gmail.com>
2025-06-19 17:13:05 +0530

    Type Casting Final Commit

---
+++ typeCasting.py
@@ -0,0 +1,10 @@
+a = "1"
+b = "2"
+print(a+b)  # Concatenation of strings, output will be '12'
+
+print(int(a) + int(b))  # Explicit Type casting to integers, output will be 3
+
+
+c = 1.9
+d = 8
+print(c + d)  # Implicit Type casting, output will be 9.9
